b8c7187c42aad9bc39eed8379ff96d7e155c33d6
Cmb <71194682+29cmb@users.noreply.github.com>
2024-12-15 10:43:09 -0500

    crafting stuff

--- src/main/java/devcmb/petsplus/datagen/ModRecipeProvider.java
+++ src/main/java/devcmb/petsplus/datagen/ModRecipeProvider.java
@@ -1,8 +1,14 @@
 package devcmb.petsplus.datagen;
 
+import devcmb.petsplus.block.ModBlocks;
+import devcmb.petsplus.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +19,23 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.PET_CORE)
+                .pattern("GGG")
+                .pattern("GEG")
+                .pattern("GGG")
+                .input('G', Items.GOLD_INGOT.asItem())
+                .input('E', Items.EGG)
+                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
+                .offerTo(exporter);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.PET_MANAGER)
+                .pattern("RRR")
+                .pattern("RCR")
+                .pattern("OOO")
+                .input('R', Blocks.REDSTONE_BLOCK.asItem())
+                .input('C', ModItems.PET_CORE)
+                .input('O', Blocks.OBSIDIAN.asItem())
+                .criterion(hasItem(ModItems.PET_CORE), conditionsFromItem(ModItems.PET_CORE))
+                .offerTo(exporter);
     }
 }
